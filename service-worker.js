609415987183f62dbe3900d6e928281d3b2957f8
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-06-24 18:53:09 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.88d800e966d84042c6abf6a248439878.js"
 );
 
 self.addEventListener('message', (event) => {
